9f5a4b44c6d517b070969a1702137c39c5963d05
nurrochman-ramadhon <nurrochman.acc@gmail.com>
2024-03-27 07:08:15 +0700

    responsive

--- src/components/card/SloganCard.jsx
+++ src/components/card/SloganCard.jsx
@@ -6,7 +6,7 @@ const SloganCardLeftImg = (props) => {
     <div className="flex flex-col mt-5 md:mt-0 md:flex-row px-[27px] md:px-0">
       <Image src={src} alt={`Slogan ${index} media`} className="md:w-[540px]" />
       <div className="md:w-[540px] md:px-[27px] flex flex-col justify-center shadow-sm px-4 py-2 md:shadow-none md:p-0 rounded-md md:rounded-none">
-        <p className="italic font-bold text-lg md:text-[26px] text-primary2 mt-3 md:mt-0 md:leading-[31.2px]">
+        <p className="italic font-bold text-lg md:text-[26px] text-primary2 mt-3 md:mt-0 md:leading-[31.2px] text-center md:text-left">
           {`${title}`.toUpperCase()}
         </p>
         <p className="text-primary1 text-sm md:text-[15px] mt-[14px] md:leading-[21px] font-medium text-justify">
@@ -22,7 +22,7 @@ const SloganCardRightImg = (props) => {
   return (
     <div className="flex flex-col-reverse mt-5 md:mt-0 md:flex-row px-[27px] md:px-0">
       <div className="md:w-[540px] md:px-[27px] flex flex-col justify-center shadow-sm px-4 py-2 md:shadow-none md:p-0">
-        <p className="italic font-bold text-lg md:text-[26px] text-primary2 mt-3 md:mt-0 md:leading-[31.2px] rounded-md md:rounded-none"> 
+        <p className="italic font-bold text-lg md:text-[26px] text-primary2 mt-3 md:mt-0 md:leading-[31.2px] rounded-md md:rounded-none text-center md:text-left"> 
           {`${title}`.toUpperCase()}
         </p>
         <p className="text-primary1 text-sm md:text-[15px] mt-[14px] md:leading-[21px] font-medium text-justify">
